c3d577beabcca75682658a0476a496e26825e1df
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2025-01-12 16:47:20 +0530

    productionurl

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,8 @@ import axios from "axios";
 import { RecoilRoot } from "recoil";
 import {ToastContainer} from "react-toastify"
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1/";
+const isProductionurl=process.env.NODE_ENV==='production'?"https://quizbackend1-knln.onrender.com":"http://localhost:3000/api/v1/"
+axios.defaults.baseURL = isProductionurl;
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
