14910eb0892de2641fdb8f15d350b00a6a6e13bb
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2025-01-12 16:42:57 +0530

    check

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 export const connectToDatabase=async ()=>{
     try{
     await connect(process.env.MONGOURL)
-    console.log("Connected to database")
+    console.log("Connected to database..")
     }catch(error){
         console.log("Cannot connect to database",error.message)
     }
