6d9131ec490c24f789da86c8d02c5eb49fe24517
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2025-01-12 16:30:12 +0530

    port fix

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
-const PORT = 5000;
+const PORT =process.env.PORT || 5000;
 
 connectToDatabase()
   .then(() => {
