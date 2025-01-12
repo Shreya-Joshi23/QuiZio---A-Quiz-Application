b1e26d6d8b209f7405f8e3ae34c3d2be33495e28
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2025-01-12 15:18:20 +0530

    database fine

---
+++ server/db.js
@@ -0,0 +1,10 @@
+import { connect } from "mongoose";
+
+export const connectToDatabase=async ()=>{
+    try{
+    await connect(process.env.MONGOURL)
+    console.log("Connected to database")
+    }catch(error){
+        console.log("Cannot connect to database",error.message)
+    }
+}
